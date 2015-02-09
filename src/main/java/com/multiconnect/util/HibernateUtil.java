eb1774c69bfda4ca910de3f92fa4382b68fad6e4
Дмитрий <@10.20.251.197>
2015-02-09 21:14:46 +0200

    bug fixes

--- src/main/java/com/multiconnect/util/HibernateUtil.java
+++ src/main/java/com/multiconnect/util/HibernateUtil.java
@@ -4,28 +4,36 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HibernateUtil {
-	private static final SessionFactory sessionFactory;
-	private static final ServiceRegistry serviceRegistry;
-	static {
-		try {
-			Configuration configuration = new Configuration();
-			configuration.configure();
-			serviceRegistry = new ServiceRegistryBuilder().applySettings(
-					configuration.getProperties()).buildServiceRegistry();
-			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-		} catch (Throwable ex) {
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+    
+    static final Logger log = LoggerFactory.getLogger(HibernateUtil.class);
+    private static final SessionFactory sessionFactory;
+    private static final ServiceRegistry serviceRegistry;
 
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-	
-	public static void closeSessionFactory() {
-        if (sessionFactory != null)
-            sessionFactory.close();        
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(
+                    configuration.getProperties()).buildServiceRegistry();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            log.debug("sessionFactory={}", sessionFactory);
+        } catch (Throwable ex) {
+            log.error("Error HibernateUtil: {}", ex.toString());
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void closeSessionFactory() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
     }
 }
